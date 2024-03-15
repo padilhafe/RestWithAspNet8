31b014d56baafd324655e38994d225e839612b99
padilhafe <62897335+padilhafe@users.noreply.github.com>
2024-03-15 17:32:25 -0300

    Session 7

---
+++ 01_RestWithAspNet8/RestWithAspNet8/RestWithAspNet8/Controllers/PersonController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithAspNet8.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+    }
+}
--- 01_RestWithAspNet8/RestWithAspNet8/RestWithAspNet8/obj/Debug/net8.0/RestWithAspNet8.AssemblyInfo.cs
+++ 01_RestWithAspNet8/RestWithAspNet8/RestWithAspNet8/obj/Debug/net8.0/RestWithAspNet8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestWithAspNet8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffa765744aaed78e700d85592582642b9f003a8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc9c7db365a325bec201ff0144fca3b7f841d038")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestWithAspNet8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestWithAspNet8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
